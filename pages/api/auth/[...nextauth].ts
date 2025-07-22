bdad3a1d2aa66ca9f9174a777dd3b2d49e950dbc
Георгий <Georgii@Mac-mini-Shoma.local>
2025-07-22 17:45:44 +0500

    Создан файл nextauth.ts с авторизацией GitHub

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,26 +1,19 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
-export default NextAuth({
+export const authOptions = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          scope: "openid email profile https://www.googleapis.com/auth/photoslibrary.readonly"
-        }
-      }
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
   callbacks: {
-    async jwt({ token, account }) {
-      if (account) token.accessToken = account.access_token;
-      return token;
-    },
     async session({ session, token }) {
-      session.accessToken = token.accessToken;
+      session.accessToken = token.accessToken as string;
       return session;
     },
   },
-});
+};
+
+export default NextAuth(authOptions);
